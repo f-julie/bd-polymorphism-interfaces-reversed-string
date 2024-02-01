cc7801f6e73bc7509669f65c0b25591eae9131a2
Julie Fiess <juliefiess@gmail.com>
2024-02-01 16:03:25 -0700

    Passing 1 of 13 tests

--- src/com/amazon/ata/interfaces/ReversedString.java
+++ src/com/amazon/ata/interfaces/ReversedString.java
@@ -5,8 +5,7 @@ package com.amazon.ata.interfaces;
  * <p>
  * Change this class definition to implement CharSequence.
  */
-public class ReversedString {
-
+public class ReversedString implements CharSequence {
 
     /**
      * Implement this constructor during the interfaces prework.
@@ -14,6 +13,58 @@ public class ReversedString {
      * @param chars The String of chars to be reversed
      */
     public ReversedString(final String chars) {
+        if (chars == null || chars.isEmpty()) {
+            throw new IllegalArgumentException("Input cannot be null or empty");
+        }
+
+        //String reversed = new StringBuilder(chars).reverse().toString();
+
+        String reversed = "";
+
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            reversed = reversed + chars.charAt(i);
+        }
     }
 
+    @Override
+    public int length() {
+        String chars = "";
+        int count = 0;
+        for (char c : chars.toCharArray()) {
+            count++;
+        }
+        return count;
+    }
+
+    @Override
+    public char charAt(int index) {
+        String input = "";
+        if (index < 0 || index >= input.length()) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        char[] chars = input.toCharArray();
+        return chars[index];
+        //return -1;  // return -1 if the character was not found
+
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        String input = "";
+        if (start < 0 || end > input.length() || start > end) {
+            throw new IllegalArgumentException("Invalid start or end index");
+        }
+        char[] chars = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i < end; i++) {
+            sb.append(chars[i]);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        String chars = "";
+        return chars;
+    }
 }
